8bb73024b8089171cff19f87170df04ddda05b6d
ratiotile <eujain@gmail.com>
2019-01-24 20:52:55 -0500

    installed eslint and flow.

--- src/components/production-menu.js
+++ src/components/production-menu.js
@@ -1,11 +1,22 @@
+//@flow
 import React from 'react'
+import {css} from 'glamor'
 
 import production_img from '../images/Production.png'
 
-export class ProductionMenu extends React.Component {
+const image_css = css({
+  display: 'inline-block',
+  width: '100%',
+  height: '100%',
+  padding: 0,
+  margin: 0,
+  verticalAlign: 'middle',
+})
+
+export class ProductionMenu extends React.Component<{}> {
   render() {
     return (
-      <span>[Production Menu]</span>
+      <img src={production_img} className={image_css} />
     )
   }
 }
